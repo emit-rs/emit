8f59a970fdfe318a3d8a2fbaa7d751389c45352f
Nicholas Blumhardt <nblumhardt@nblumhardt.com>
2016-06-22 14:39:51 +1000

    Tiny advancement - remove static bound on FixedPropertyEnricher property name (#12)

--- src/enrichers/fixed_property/mod.rs
+++ src/enrichers/fixed_property/mod.rs
@@ -3,18 +3,18 @@ use events::Event;
 use pipeline::chain::{ChainedElement,PipelineElement};
 use serde;
 
-pub struct FixedPropertyEnricher {
-    name: &'static str,
+pub struct FixedPropertyEnricher<'a> {
+    name: &'a str,
     value: String
 }
 
-impl FixedPropertyEnricher {
-    pub fn new<T: serde::ser::Serialize>(name: &'static str, value: &T) -> FixedPropertyEnricher {
+impl<'a> FixedPropertyEnricher<'a> {
+    pub fn new<T: serde::ser::Serialize>(name: &'a str, value: &T) -> FixedPropertyEnricher<'a> {
         FixedPropertyEnricher { name: name, value: events::capture_property_value(value) }
     }
 }
 
-impl PipelineElement for FixedPropertyEnricher {
+impl PipelineElement for FixedPropertyEnricher<'static> {
     fn emit(&self, event: Event<'static>, next: &ChainedElement) {
         let mut e = event;
         e.add_or_update_property(self.name, self.value.clone());
