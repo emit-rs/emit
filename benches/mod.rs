b103fe0d4e229c30b70d267352ef93e426408c3a
Ashley Mannix <ashleymannix@live.com.au>
2016-06-23 16:35:56 +1000

    fix failing benches

--- benches/mod.rs
+++ benches/mod.rs
@@ -12,11 +12,18 @@ pub fn template_repl(b: &mut Bencher) {
 	let template = templates::repl::MessageTemplateRepl::new("Some value A: {A} And some other value: {Bert} There are no more values to parse");
 	let mut map = BTreeMap::new();
 
-    map.insert("A", "value1");
-    map.insert("Bert", "value2");
+    map.insert("A", "value1".into());
+    map.insert("Bert", "value2".into());
 
 	b.iter(|| {
 		template.replace(&map)
+	});
+}
+
+#[bench]
+pub fn template_repl_new(b: &mut Bencher) {
+	b.iter(|| {
+		templates::repl::MessageTemplateRepl::new("Some value A: {A} And some other value: {Bert} There are no more values to parse")
 	});
 }
 
@@ -28,15 +35,8 @@ pub fn template_from_format(b: &mut Bencher) {
 }
 
 #[bench]
-pub fn template_from_new(b: &mut Bencher) {
+pub fn template_new(b: &mut Bencher) {
 	b.iter(|| {
 		templates::MessageTemplate::new("Some value A: {A} And some other value: {Bert} There are no more values to parse")
-	});
-}
-
-#[bench]
-pub fn template_fmt(b: &mut Bencher) {
-	b.iter(|| {
-		format!("Some value A: {A} And some other value: {Bert} There are no more values to parse", A="value1", Bert="value2")
 	});
 }
