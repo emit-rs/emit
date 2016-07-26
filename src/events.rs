5e7382fc621b0a7d526c1a0a21821ab9b5498693
Ashley Mannix <ashleymannix@live.com.au>
2016-07-27 08:40:44 +1000

    refactor value, add vec

--- src/events.rs
+++ src/events.rs
@@ -6,24 +6,25 @@ use templates;
 use std::fmt;
 use std::convert::Into;
 
-/// Currently just used as a marker; plan is to
-/// adopt the same scheme as serde_json's Value: https://github.com/serde-rs/json/blob/master/json/src/value.rs
 #[derive(Clone, PartialEq)]
 pub enum Value {
-    /// Represents a JSON Boolean
+    /// Represents a Boolean
     Bool(bool),
 
-    /// Represents a JSON signed integer
+    /// Represents a signed integer
     I64(i64),
 
-    /// Represents a JSON unsigned integer
+    /// Represents an unsigned integer
     U64(u64),
 
-    /// Represents a JSON floating point number
+    /// Represents a floating point number
     F64(f64),
 
-    /// Represents a JSON string
+    /// Represents a string
     String(String),
+
+    /// Represents a collection
+    Vec(Vec<Value>)
 }
 
 impl Value {
@@ -42,6 +43,37 @@ impl Value {
                 quoted.push('"');
 
                 quoted
+            },
+            Value::Vec(ref v) => {
+                let mut len = 0;
+                let mut results = Vec::with_capacity(v.len());
+
+                for val in v {
+                    let res = val.to_json();
+                    len += res.len();
+                    results.push(res);
+                }
+
+                let mut json = String::with_capacity(
+                    len + //item data
+                    2 + //brackets
+                    results.len() - 1 //commas
+                );
+
+                let mut first = true;
+                json.push('[');
+                for res in results {
+                    if !first {
+                        json.push(',');
+                    }
+                    else {
+                        first = false;
+                    }
+                    json.push_str(res.as_ref());
+                }
+                json.push(']');
+
+                json
             }
         }
     }
@@ -54,7 +86,8 @@ impl fmt::Debug for Value {
             Value::I64(ref n) => write!(f, "I64({})", n),
             Value::U64(ref n) => write!(f, "U64({})", n),
             Value::F64(ref n) => write!(f, "F64({})", n),
-            Value::String(ref s) => write!(f, "String(\"{}\")", s)
+            Value::String(ref s) => write!(f, "String(\"{}\")", s),
+            Value::Vec(_) => write!(f, "Vec()")
         }
     }
 }
@@ -66,87 +99,98 @@ impl fmt::Display for Value {
             Value::I64(ref n) => write!(f, "{}", n),
             Value::U64(ref n) => write!(f, "{}", n),
             Value::F64(ref n) => write!(f, "{}", n),
-            Value::String(ref s) => write!(f, "{}", s)
+            Value::String(ref s) => write!(f, "{}", s),
+            Value::Vec(_) => write!(f, "vec[]")
         }
     }
 }
 
-impl Into<Value> for bool {
-    fn into(self) -> Value {
+trait IntoValue {
+    fn into_value(self) -> Value;
+}
+
+impl IntoValue for bool {
+    fn into_value(self) -> Value {
         Value::Bool(self)
     }
 }
 
-impl Into<Value> for i64 {
-    fn into(self) -> Value {
+impl IntoValue for i64 {
+    fn into_value(self) -> Value {
         Value::I64(self)
     }
 }
 
-impl Into<Value> for i8 {
-    fn into(self) -> Value {
+impl IntoValue for i8 {
+    fn into_value(self) -> Value {
         Value::I64(self.into())
     }
 }
 
-impl Into<Value> for i16 {
-    fn into(self) -> Value {
+impl IntoValue for i16 {
+    fn into_value(self) -> Value {
         Value::I64(self.into())
     }
 }
 
-impl Into<Value> for i32 {
-    fn into(self) -> Value {
+impl IntoValue for i32 {
+    fn into_value(self) -> Value {
         Value::I64(self.into())
     }
 }
 
-impl Into<Value> for u64 {
-    fn into(self) -> Value {
+impl IntoValue for u64 {
+    fn into_value(self) -> Value {
         Value::U64(self)
     }
 }
 
-impl Into<Value> for u8 {
-    fn into(self) -> Value {
+impl IntoValue for u8 {
+    fn into_value(self) -> Value {
         Value::U64(self.into())
     }
 }
 
-impl Into<Value> for u16 {
-    fn into(self) -> Value {
+impl IntoValue for u16 {
+    fn into_value(self) -> Value {
         Value::U64(self.into())
     }
 }
 
-impl Into<Value> for u32 {
-    fn into(self) -> Value {
+impl IntoValue for u32 {
+    fn into_value(self) -> Value {
         Value::U64(self.into())
     }
 }
 
-impl Into<Value> for f64 {
-    fn into(self) -> Value {
+impl IntoValue for f64 {
+    fn into_value(self) -> Value {
         Value::F64(self)
     }
 }
 
-impl Into<Value> for f32 {
-    fn into(self) -> Value {
+impl IntoValue for f32 {
+    fn into_value(self) -> Value {
         Value::F64(self.into())
     }
 }
 
 
-impl Into<Value> for String {
-    fn into(self) -> Value {
+impl IntoValue for String {
+    fn into_value(self) -> Value {
         Value::String(self)
     }
 }
 
-impl<'a> Into<Value> for &'a str {
-    fn into(self) -> Value {
+impl<'a> IntoValue for &'a str {
+    fn into_value(self) -> Value {
         Value::String(self.into())
+    }
+}
+
+impl <V: IntoValue> IntoValue for Vec<V> {
+    fn into_value(self) -> Value {
+        Value::Vec(self.into_iter().map(|v| v.into_value()).collect())
     }
 }
 
