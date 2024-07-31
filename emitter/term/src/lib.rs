3d050a139c0eb4161ad0375b71bfe6b3fe6b03b0
KodrAus <kodraus@hey.com>
2024-08-01 08:48:42 +1000

    prepare for 0.11.0-alpha.7 release

--- emitter/term/src/lib.rs
+++ emitter/term/src/lib.rs
@@ -9,10 +9,10 @@ Add `emit` and `emit_term` to your `Cargo.toml`:
 
 ```toml
 [dependencies.emit]
-version = "0.11.0-alpha.6"
+version = "0.11.0-alpha.7"
 
 [dependencies.emit_term]
-version = "0.11.0-alpha.6"
+version = "0.11.0-alpha.7"
 ```
 
 Initialize `emit` using `emit_term`:
--- src/setup.rs
+++ src/setup.rs
@@ -7,7 +7,7 @@ You can implement your own runtime, providing your own implementations of the am
 
 ```toml
 [dependencies.emit]
-version = "0.11.0-alpha.6"
+version = "0.11.0-alpha.7"
 default-features = false
 features = ["std"]
 ```
