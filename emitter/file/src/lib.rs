29ae8dd350a394c6b2dbe4b6315236dba2425209
KodrAus <kodraus@hey.com>
2024-08-15 16:48:40 +1000

    prepare for 0.11.0-alpha.8 release

--- emitter/file/src/lib.rs
+++ emitter/file/src/lib.rs
@@ -15,10 +15,10 @@ Add `emit` and `emit_file` to your `Cargo.toml`:
 
 ```toml
 [dependencies.emit]
-version = "0.11.0-alpha.7"
+version = "0.11.0-alpha.8"
 
 [dependencies.emit_file]
-version = "0.11.0-alpha.7"
+version = "0.11.0-alpha.8"
 ```
 
 Initialize `emit` using a rolling file set:
