0ea8dadfdbacfe1cdddf0cc6126b6d20ec320bd2
KodrAus <kodraus@hey.com>
2024-08-21 21:44:51 +1000

    include other protocols in the integration test

--- emitter/otlp/test/integration/src/main.rs
+++ emitter/otlp/test/integration/src/main.rs
@@ -2,27 +2,72 @@
 An integration test between `emit_otlp` and the OpenTelemetry Collector.
 */
 
+use emit::Emitter;
+
 use std::{
     io::Read,
     process::{Child, Command, Stdio},
 };
 
 fn main() {
-    // Configure `emit_otlp`
-    let rt = emit::setup()
-        .emit_to(
-            emit_otlp::new()
-                .resource(emit::props! {
-                    #[emit::key("service.name")]
-                    service_name: emit::pkg!(),
-                })
-                .logs(emit_otlp::logs_grpc_proto("http://localhost:44319"))
-                .traces(emit_otlp::traces_grpc_proto("http://localhost:44319"))
-                .metrics(emit_otlp::metrics_grpc_proto("http://localhost:44319"))
-                .spawn()
-                .unwrap(),
-        )
-        .init();
+    // So ambient methods for generating ids and timestamps will work
+    let _ = emit::setup().init();
+
+    assert_emitter(
+        "gRPC proto",
+        emit_otlp::new()
+            .resource(emit::props! {
+                #[emit::key("service.name")]
+                service_name: emit::pkg!(),
+            })
+            .logs(emit_otlp::logs_grpc_proto("http://localhost:44319"))
+            .traces(emit_otlp::traces_grpc_proto("http://localhost:44319"))
+            .metrics(emit_otlp::metrics_grpc_proto("http://localhost:44319"))
+            .spawn()
+            .unwrap(),
+    );
+
+    assert_emitter(
+        "HTTP proto",
+        emit_otlp::new()
+            .resource(emit::props! {
+                #[emit::key("service.name")]
+                service_name: emit::pkg!(),
+            })
+            .logs(emit_otlp::logs_http_proto("http://localhost:44318/v1/logs"))
+            .traces(emit_otlp::traces_http_proto(
+                "http://localhost:44318/v1/traces",
+            ))
+            .metrics(emit_otlp::metrics_http_proto(
+                "http://localhost:44318/v1/metrics",
+            ))
+            .spawn()
+            .unwrap(),
+    );
+
+    assert_emitter(
+        "HTTP JSON",
+        emit_otlp::new()
+            .resource(emit::props! {
+                #[emit::key("service.name")]
+                service_name: emit::pkg!(),
+            })
+            .logs(emit_otlp::logs_http_json("http://localhost:44318/v1/logs"))
+            .traces(emit_otlp::traces_http_json(
+                "http://localhost:44318/v1/traces",
+            ))
+            .metrics(emit_otlp::metrics_http_json(
+                "http://localhost:44318/v1/metrics",
+            ))
+            .spawn()
+            .unwrap(),
+    );
+}
+
+fn assert_emitter(name: &str, emitter: impl emit::Emitter + Send + Sync + 'static) {
+    println!("checking {name}");
+
+    let rt = emit::runtime::Runtime::new().with_emitter(emitter);
 
     // Start the collector
     let otelcol = OtelCol::spawn();
@@ -34,33 +79,30 @@ fn main() {
     let metric_uuid = uuid::Uuid::new_v4().to_string();
 
     // Emit a log event
-    emit::info!("A log message {log_uuid}");
+    emit::info!(rt, "A log message {log_uuid}");
 
     // Emit a span in a trace
-    #[emit::span("A span {span_uuid}")]
-    fn span(span_uuid: &str) {}
-    span(&span_uuid);
+    emit::emit!(rt, extent: emit::now()..emit::now(), "A span {span_uuid}", evt_kind: "span", span_name: "emit_otlp_test", trace_id: emit::new_trace_id(), span_id: emit::new_span_id());
 
     // Emit a metric
-    emit::emit!("A metric {metric_uuid}", evt_kind: "metric", metric_name: "emit_otlp_test", metric_agg: "count", metric_value: 1);
+    emit::emit!(rt, "A metric {metric_uuid}", evt_kind: "metric", metric_name: "emit_otlp_test", metric_agg: "count", metric_value: 1);
 
     // Flush `emit_otlp` and read the output from the collector
     rt.blocking_flush(std::time::Duration::from_secs(10));
     let output = otelcol.output();
-    println!("{output}");
 
     // Ensure the collector received and accepted the events we emitted
-    assert_emitted(&output, "LogsExporter", &log_uuid);
-    assert_emitted(&output, "TracesExporter", &span_uuid);
-    assert_emitted(&output, "MetricsExporter", &metric_uuid);
+    assert_exporter(&output, "LogsExporter", &log_uuid);
+    assert_exporter(&output, "TracesExporter", &span_uuid);
+    assert_exporter(&output, "MetricsExporter", &metric_uuid);
 }
 
-fn assert_emitted(output: &str, exporter: &str, id: &str) {
+fn assert_exporter(output: &str, exporter: &str, id: &str) {
     assert!(
         output.contains(&exporter),
-        "{exporter} not found in otelcol output"
+        "{exporter} not found in:\n{output}"
     );
-    assert!(output.contains(id), "{id} noot found in otelcol output");
+    assert!(output.contains(id), "{id} noot found in:\n{output}");
 }
 
 struct OtelCol(Child);
--- src/lib.rs
+++ src/lib.rs
@@ -640,6 +640,16 @@ pub fn clock() -> runtime::AmbientClock<'static> {
 }
 
 /**
+Get the current timestamp.
+
+This method will use the [`Clock`] from [`runtime::shared()`].
+*/
+#[cfg(feature = "implicit_rt")]
+pub fn now() -> Option<Timestamp> {
+    clock().now()
+}
+
+/**
 Get the shared context.
 
 This method will use the [`Ctxt`] from [`runtime::shared()`].
@@ -659,6 +669,26 @@ This method will use the [`Rng`] from [`runtime::shared()`].
 #[cfg(feature = "implicit_rt")]
 pub fn rng() -> runtime::AmbientRng<'static> {
     *runtime::shared().rng()
+}
+
+/**
+Get a new, random trace id.
+
+This method will use the [`Rng`] from [`runtime::shared()`].
+*/
+#[cfg(feature = "implicit_rt")]
+pub fn new_trace_id() -> Option<span::TraceId> {
+    span::TraceId::random(rng())
+}
+
+/**
+Get a new, random span id.
+
+This method will use the [`Rng`] from [`runtime::shared()`].
+*/
+#[cfg(feature = "implicit_rt")]
+pub fn new_span_id() -> Option<span::SpanId> {
+    span::SpanId::random(rng())
 }
 
 #[doc(hidden)]
