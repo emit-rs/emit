8e5b49bb80896f91be09074f55c426002b8238ca
KodrAus <kodraus@hey.com>
2024-08-21 21:20:30 +1000

    add some comments to the integration test

--- emitter/otlp/test/integration/src/main.rs
+++ emitter/otlp/test/integration/src/main.rs
@@ -1,9 +1,14 @@
+/*!
+An integration test between `emit_otlp` and the OpenTelemetry Collector.
+*/
+
 use std::{
     io::Read,
     process::{Child, Command, Stdio},
 };
 
 fn main() {
+    // Configure `emit_otlp`
     let rt = emit::setup()
         .emit_to(
             emit_otlp::new()
@@ -19,27 +24,32 @@ fn main() {
         )
         .init();
 
+    // Start the collector
     let otelcol = OtelCol::spawn();
 
+    // Generate some random ids
+    // These are used to assert the collector received our events
     let log_uuid = uuid::Uuid::new_v4().to_string();
     let span_uuid = uuid::Uuid::new_v4().to_string();
     let metric_uuid = uuid::Uuid::new_v4().to_string();
 
+    // Emit a log event
     emit::info!("A log message {log_uuid}");
 
+    // Emit a span in a trace
     #[emit::span("A span {span_uuid}")]
     fn span(span_uuid: &str) {}
-
     span(&span_uuid);
 
+    // Emit a metric
     emit::emit!("A metric {metric_uuid}", evt_kind: "metric", metric_name: "emit_otlp_test", metric_agg: "count", metric_value: 1);
 
+    // Flush `emit_otlp` and read the output from the collector
     rt.blocking_flush(std::time::Duration::from_secs(10));
-
     let output = otelcol.output();
-
     println!("{output}");
 
+    // Ensure the collector received and accepted the events we emitted
     assert_emitted(&output, "LogsExporter", &log_uuid);
     assert_emitted(&output, "TracesExporter", &span_uuid);
     assert_emitted(&output, "MetricsExporter", &metric_uuid);
