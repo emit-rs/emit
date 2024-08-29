32fb2af45e45b217ceab3523137777600c4643ff
KodrAus <kodraus@hey.com>
2024-08-29 18:02:22 +1000

    prepare for 0.11.0-alpha.13 release

--- src/setup.rs
+++ src/setup.rs
@@ -7,7 +7,7 @@ You can implement your own runtime, providing your own implementations of the am
 
 ```toml
 [dependencies.emit]
-version = "0.11.0-alpha.12"
+version = "0.11.0-alpha.13"
 default-features = false
 features = ["std"]
 ```
