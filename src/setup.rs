609b910645ec1c5addcf7cb0ebb6be3c3afc12d8
Ashley Mannix <ashleymannix@live.com.au>
2024-09-11 09:49:22 +1000

    prepare for 0.11.0-alpha.16 release

--- src/setup.rs
+++ src/setup.rs
@@ -7,7 +7,7 @@ You can implement your own runtime, providing your own implementations of the am
 
 ```toml
 [dependencies.emit]
-version = "0.11.0-alpha.15"
+version = "0.11.0-alpha.16"
 default-features = false
 features = ["std"]
 ```
