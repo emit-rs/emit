275cb3ac3d52ea82a30230cc0c4452a2307f625c
KodrAus <kodraus@hey.com>
2025-01-23 06:51:30 +1000

    prepare for 0.11.6 release

--- emitter/file/src/lib.rs
+++ emitter/file/src/lib.rs
@@ -15,10 +15,10 @@ Add `emit` and `emit_file` to your `Cargo.toml`:
 
 ```toml
 [dependencies.emit]
-version = "0.11.5"
+version = "0.11.6"
 
 [dependencies.emit_file]
-version = "0.11.5"
+version = "0.11.6"
 ```
 
 Initialize `emit` using a rolling file set:
