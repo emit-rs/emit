3dfec86d9d134d3e2b3ae04698968ecf8936f13a
KodrAus <kodraus@hey.com>
2025-02-03 07:53:16 +1000

    prepare for 0.11.8 release

--- emitter/file/src/lib.rs
+++ emitter/file/src/lib.rs
@@ -15,10 +15,10 @@ Add `emit` and `emit_file` to your `Cargo.toml`:
 
 ```toml
 [dependencies.emit]
-version = "0.11.7"
+version = "0.11.8"
 
 [dependencies.emit_file]
-version = "0.11.7"
+version = "0.11.8"
 ```
 
 Initialize `emit` using a rolling file set:
--- src/setup.rs
+++ src/setup.rs
@@ -7,7 +7,7 @@ You can implement your own runtime, providing your own implementations of the am
 
 ```toml
 [dependencies.emit]
-version = "0.11.7"
+version = "0.11.8"
 default-features = false
 features = ["std"]
 ```
