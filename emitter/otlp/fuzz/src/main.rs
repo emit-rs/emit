3531b772b1de601a1d4f5b13d7cc5466bfb3a62c
KodrAus <kodraus@hey.com>
2025-04-21 16:11:30 +1000

    build afl behind a feature

--- emitter/otlp/fuzz/src/main.rs
+++ emitter/otlp/fuzz/src/main.rs
@@ -8,17 +8,18 @@ mod baggage;
 
 use self::error::*;
 
-use std::str;
-
 fn main() {
-    afl::fuzz!(|data: &[u8]| {
-        let Ok(s) = str::from_utf8(data) else {
-            return;
-        };
+    #[cfg(feature = "afl")]
+    {
+        afl::fuzz!(|data: &[u8]| {
+            let Ok(s) = std::str::from_utf8(data) else {
+                return;
+            };
 
-        // Just ensure we don't panic
-        let _ = baggage::parse(s);
-    });
+            // Just ensure we don't panic
+            let _ = baggage::parse(s);
+        });
+    }
 }
 
 #[cfg(test)]
