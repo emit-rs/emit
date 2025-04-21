26f92f4735111aa93cf9dbb577343b10817bba1a
KodrAus <kodraus@hey.com>
2025-04-21 21:55:24 +1000

    add more docs to env var config

--- examples/opentelemetry/direct_otlp/src/main.rs
+++ examples/opentelemetry/direct_otlp/src/main.rs
@@ -2,21 +2,24 @@
 This example demonstrates configuring `emit` to produce OTLP directly.
 
 You can point `emit` directly at an OpenTelemetry Collector or other compatible service.
+
+`emit_otlp` can be configured manually, or via OpenTelemetry environment variables.
+In this case, we're using environment variables, which will default to emitting via gRPC to `http://locslhost:4317`.
+
+Try setting:
+
+- `OTEL_EXPORTER_OTLP_ENDPOINT` and `OTEL_EXPORTER_OTLP_PROTOCOL` to specify where diagnostics go, and the protocol.
+- `OTEL_SERVICE_NAME` to specify the name of the service.
+- `OTEL_RESOURCE_ATTRIBUTES` to add ambient properties.
+
+See [the docs](https://docs.rs/emit_otlp/1.6.0/emit_otlp/index.html#configuring-from-environment-variables) for more details on supported environment variables.
 */
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let _ = emit::setup().emit_to(emit_term::stdout()).init_internal();
 
     // Configure `emit` to produce OTLP
-    let rt = emit::setup()
-        .emit_to(
-            emit_otlp::new()
-                .logs(emit_otlp::logs_grpc_proto("http://localhost:4319"))
-                .traces(emit_otlp::traces_grpc_proto("http://localhost:4319"))
-                .metrics(emit_otlp::metrics_grpc_proto("http://localhost:4319"))
-                .spawn(),
-        )
-        .init();
+    let rt = emit::setup().emit_to(emit_otlp::from_env().spawn()).init();
 
     run();
 
