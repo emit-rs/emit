209a34139d77316466f9b6d48dd3ad8d615051eb
KodrAus <kodraus@hey.com>
2025-06-21 08:48:35 +1000

    add flushing to web example

--- examples/opentelemetry/web/src/lib.rs
+++ examples/opentelemetry/web/src/lib.rs
@@ -1,10 +1,16 @@
 use wasm_bindgen::prelude::*;
 
+/*
+Configure `emit`.
+
+The shape of this function is likely going to be similar in all web applications.
+We'll call this early on in our `index.html`.
+*/
 #[wasm_bindgen]
-pub fn setup() {
+pub fn setup() -> Setup {
     std::panic::set_hook(Box::new(console_error_panic_hook::hook));
 
-    let _ = emit::setup()
+    let rt = emit::setup()
         .emit_to(
             emit_otlp::new()
                 .resource(emit::props! {
@@ -21,8 +27,36 @@ pub fn setup() {
                 .spawn(),
         )
         .try_init();
+
+    // Return a value we can use to flush later
+    Setup {
+        otlp: rt.map(|rt| rt.emitter()),
+    }
 }
 
+/*
+This type is a wrapper around a configured `Otlp` emitter that we can flush asynchronously.
+
+`emit` itself only exposes synchronous flushing, but `emit_otlp` has an asynchronous flush method.
+*/
+#[wasm_bindgen]
+pub struct Setup {
+    otlp: Option<&'static emit_otlp::Otlp>,
+}
+
+#[wasm_bindgen]
+impl Setup {
+    #[wasm_bindgen]
+    pub async fn flush(&self) {
+        if let Some(ref otlp) = self.otlp {
+            otlp.flush(std::time::Duration::from_secs(5)).await;
+        }
+    }
+}
+
+/*
+Do some work, emitting events along the way.
+*/
 #[wasm_bindgen]
 pub fn run() {
     emit::debug!("Hello {user}", user: "Web");
