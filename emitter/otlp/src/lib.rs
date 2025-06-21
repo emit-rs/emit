4e2deaedbe42c3be4763bbcaf69e4ba09290f6e7
KodrAus <kodraus@hey.com>
2025-06-21 19:15:02 +1000

    prepare for 1.11.0 release

--- emitter/otlp/src/lib.rs
+++ emitter/otlp/src/lib.rs
@@ -38,10 +38,10 @@ Add `emit` and `emit_otlp` to your `Cargo.toml`:
 
 ```toml
 [dependencies.emit]
-version = "1.10.0"
+version = "1.11.0"
 
 [dependencies.emit_otlp]
-version = "1.10.0"
+version = "1.11.0"
 ```
 
 Initialize `emit` at the start of your `main.rs` using an OTLP emitter:
