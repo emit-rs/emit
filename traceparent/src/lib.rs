d23983aa8fecd17da9af0cc7e7fe967d88e7d7c2
KodrAus <kodraus@hey.com>
2026-01-11 15:33:22 +1000

    update traceparent docs

--- traceparent/src/lib.rs
+++ traceparent/src/lib.rs
@@ -383,6 +383,9 @@ impl Traceparent {
     Get the current trace context.
 
     If no context has been set, this method will return a new traceparent with no trace id or span id, but with [`TraceFlags::SAMPLED`].
+
+    Note that unless a [`TraceparentCtxt`] is configured in [`emit::setup`], or if trace context is being managed through [`Traceparent::push`], this method will return a new traceparent.
+    It won't pick up values in [`emit::span::SpanCtxt`].
     */
     pub fn current() -> Self {
         get_active_traceparent()
