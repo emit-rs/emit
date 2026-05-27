57be88a3144aad6fa8ca198b08034d52a074c64c
KodrAus <kodraus@hey.com>
2026-05-27 17:23:01 +1000

    clean up high volume example

--- examples/high_volume/src/main.rs
+++ examples/high_volume/src/main.rs
@@ -1,44 +1,48 @@
-use std::collections::HashMap;
-use std::sync::{LazyLock, Mutex};
-use std::thread;
-use std::time::Duration;
+/*!
+This example demonstrates mapping high frequency span events into exponential histograms.
+
+Tracing every request in an application quickly becomes unfeasible. We don't want to just
+throw away that valuable timing data though. We can use exponential histograms to compress
+that data for all spans into a form we can still compute percentiles over without having
+to store all the span events themselves.
+*/
+
+use std::{thread, time::Duration};
 
 fn main() {
     let rt = emit::setup()
         .emit_to(emit::emitter::from_fn(|evt| {
             use emit::{Emitter as _, Props as _};
 
-            if evt
-                .props()
-                .pull::<emit::Kind, _>(emit::well_known::KEY_EVT_KIND)
-                == Some(emit::Kind::Span)
-            {
+            // If we're looking at a span event then collect some timing information from it
+            if emit::kind::is_span(&evt) {
                 if let Some(elapsed) = evt.extent().and_then(|extent| extent.len()) {
-                    let key = evt.tpl().to_owned();
-
-                    let mut guard = SPAN_ELAPSED_SECS.lock().unwrap();
-
-                    guard
-                        .current_value_mut()
-                        .entry(key)
-                        .or_insert_with(|| emit::metric::exp::Distribution::default())
-                        .observe(elapsed.as_secs_f64());
+                    span_elapsed::observe(evt.tpl().to_owned(), elapsed);
                 }
 
-                if let Some(tr) = evt
+                // Filter out most spans for traces
+                //
+                // We do this here instead of as a top-level filter so the span is still
+                // constructed and timed, even if we don't intend to fully emit it.
+                if evt
                     .props()
-                    .pull::<emit::TraceId, _>(emit::well_known::KEY_TRACE_ID)
+                    .pull::<emit::TraceId, _>("trace_id")
+                    .map(|tr| tr.to_u128())
+                    .unwrap_or_default()
+                    % 30_000
+                    != 0
                 {
-                    if tr.to_u128() % 30_000 != 0 {
-                        return;
-                    }
+                    return;
                 }
             }
+
+            // If we get this far then we're emitting the event
 
             emit_term::stdout().emit(evt)
         }))
         .init();
 
+    // Construct some high-volume workers that will all produce spans
     let mut workers = Vec::new();
     for _ in 0..100 {
         workers.push(std::thread::spawn(|| {
@@ -52,24 +56,22 @@ fn main() {
         worker.join().unwrap();
     }
 
-    {
-        let mut guard = SPAN_ELAPSED_SECS.lock().unwrap();
-        let (extent, metrics) = guard.advance(emit::clock().now());
-        let metrics = std::mem::take(metrics);
-        drop(guard);
-
-        for (tpl, distribution) in metrics {
-            emit::count_sample!(
-                extent,
-                name: &tpl.to_string(),
-                value: distribution.count(),
-                props: distribution,
-            );
-        }
+    // Emit our collected timing metrics
+    //
+    // In a more sophisticated application, we could use a `Reporter` to
+    // do this regularly instead of just once at the end.
+    for (tpl, distribution) in span_elapsed::take() {
+        emit::count_sample!(
+            name: &tpl.to_string(),
+            value: distribution.count(),
+            props: distribution,
+        );
     }
 
     rt.blocking_flush(Duration::from_secs(5));
 }
+
+// The work we're going to perform
 
 #[emit::span("a")]
 fn a() {
@@ -84,7 +86,7 @@ fn a() {
 fn b() {
     thread::sleep(Duration::from_millis(rand::random_range(1..3)));
 
-    for _ in 0..rand::random_range(1..2) {
+    for _ in 0..rand::random_range(0..2) {
         c();
     }
 }
@@ -94,11 +96,32 @@ fn c() {
     thread::sleep(Duration::from_millis(rand::random_range(3..5)));
 }
 
-static SPAN_ELAPSED_SECS: LazyLock<
-    Mutex<emit::metric::Delta<HashMap<emit::Template<'static>, emit::metric::exp::Distribution>>>,
-> = LazyLock::new(|| {
-    Mutex::new(emit::metric::Delta::new(
-        emit::clock().now(),
-        HashMap::new(),
-    ))
-});
+mod span_elapsed {
+    use std::{
+        collections::HashMap,
+        sync::{LazyLock, Mutex},
+        time::Duration,
+    };
+
+    // Track a set of distributions against the event's template
+    //
+    // Templates make good keys for identifying spans, so we can distinguish the timings of different operations.
+    static SPAN_ELAPSED_SECS: LazyLock<
+        Mutex<HashMap<emit::Template<'static>, emit::metric::exp::Distribution>>,
+    > = LazyLock::new(|| Mutex::new(HashMap::new()));
+
+    pub fn observe(key: emit::Template<'static>, elapsed: Duration) {
+        let mut guard = SPAN_ELAPSED_SECS.lock().unwrap();
+
+        guard
+            .entry(key)
+            .or_insert_with(|| emit::metric::exp::Distribution::default())
+            .observe(elapsed.as_secs_f64());
+    }
+
+    pub fn take(
+    ) -> impl IntoIterator<Item = (emit::Template<'static>, emit::metric::exp::Distribution)> {
+        let mut guard = SPAN_ELAPSED_SECS.lock().unwrap();
+        std::mem::take(&mut *guard)
+    }
+}
