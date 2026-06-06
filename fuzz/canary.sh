9d26d8fb2f5e331e96526d3f9423afb09e1404a4
Ashley <kodraus@hey.com>
2026-06-06 10:12:42 +1000

    add a fuzz test for otel baggage

--- fuzz/canary.sh
+++ fuzz/canary.sh
@@ -8,11 +8,12 @@ cargo test --manifest-path fuzz/Cargo.toml
 cargo afl build --manifest-path fuzz/Cargo.toml --features afl
 
 # Add new fuzz cases here
-AFL_NO_UI=1 timeout 17s cargo afl fuzz -i fuzz/timestamp/in -o fuzz/target/timestamp fuzz/target/debug/timestamp > /dev/null 2>&1 &
-AFL_NO_UI=1 timeout 17s cargo afl fuzz -i fuzz/bucket_set/in -o fuzz/target/bucket_set fuzz/target/debug/bucket_set > /dev/null 2>&1 &
-AFL_NO_UI=1 timeout 17s cargo afl fuzz -i fuzz/span_link_set/in -o fuzz/target/span_link_set fuzz/target/debug/span_link_set > /dev/null 2>&1 &
+AFL_NO_UI=1 timeout 47s cargo afl fuzz -i fuzz/timestamp/in -o fuzz/target/timestamp fuzz/target/debug/timestamp > /dev/null 2>&1 &
+AFL_NO_UI=1 timeout 47s cargo afl fuzz -i fuzz/bucket_set/in -o fuzz/target/bucket_set fuzz/target/debug/bucket_set > /dev/null 2>&1 &
+AFL_NO_UI=1 timeout 47s cargo afl fuzz -i fuzz/span_link_set/in -o fuzz/target/span_link_set fuzz/target/debug/span_link_set > /dev/null 2>&1 &
+AFL_NO_UI=1 timeout 47s cargo afl fuzz -i fuzz/otel_baggage/in -o fuzz/target/otel_baggage fuzz/target/debug/otel_baggage > /dev/null 2>&1 &
 
 echo "waiting for fuzz run..."
-sleep 20s
+sleep 50s
 
 cargo test --manifest-path fuzz/Cargo.toml --features force
