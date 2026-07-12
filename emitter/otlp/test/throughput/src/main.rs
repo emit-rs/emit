788c5559145212643c9b8276b0e6fa4e5e04a7c0
Ashley Mannix <kodraus@hey.com>
2026-07-12 13:00:34 +1000

    tweak throughput test to find more hotspots

--- emitter/otlp/test/throughput/src/main.rs
+++ emitter/otlp/test/throughput/src/main.rs
@@ -4,13 +4,37 @@ A throughput test for emitting events via OTLP.
 This project doesn't prove much except what the on-thread cost of event serialization is like.
 */
 
-use emit::Emitter;
+use emit::{Clock, Emitter};
+use std::sync::LazyLock;
 
 use std::{
     env,
     process::{Child, Command},
     time::Duration,
 };
+
+// Concrete runtime to make `perf` output easier to follow
+static RUNTIME: LazyLock<
+    emit::runtime::Runtime<
+        emit_otlp::Otlp,
+        emit::Empty,
+        emit::platform::DefaultCtxt,
+        emit::platform::DefaultClock,
+        emit::platform::DefaultRng,
+    >,
+> = LazyLock::new(|| {
+    let emitter = emit_otlp::Otlp::builder()
+        .traces(emit_otlp::traces_grpc_proto("http://localhost:44319"))
+        .logs(emit_otlp::logs_grpc_proto("http://localhost:44319"))
+        .spawn();
+    emit::runtime::Runtime::build(
+        emitter,
+        emit::Empty,
+        emit::platform::DefaultCtxt::shared(),
+        emit::platform::DefaultClock::new(),
+        emit::platform::DefaultRng::new(),
+    )
+});
 
 fn main() {
     let stdout = emit_term::stdout();
@@ -19,42 +43,28 @@ fn main() {
     let flush = env::args().any(|a| a == "--flush");
 
     let mut reporter = emit::metric::Reporter::new();
-
-    // Set up `emit_otlp`
-    let rt = emit::setup()
-        .emit_to({
-            let emitter = emit_otlp::new()
-                .traces(emit_otlp::traces_grpc_proto("http://localhost:44319"))
-                .logs(emit_otlp::logs_grpc_proto("http://localhost:44319"))
-                .spawn();
-
-            reporter.add_source(emitter.metric_source());
-
-            emitter
-        })
-        .init();
+    reporter.add_source(RUNTIME.emitter().metric_source());
 
     let otelcol = if spawn { Some(OtelCol::spawn()) } else { None };
 
     // Emit our events
     let count = 10_000;
-    let start = emit::clock().now().unwrap();
+    let start = RUNTIME.clock().now().unwrap();
 
     root(count);
 
     if flush {
-        rt.blocking_flush(Duration::from_secs(30));
+        RUNTIME.blocking_flush(Duration::from_secs(30));
     }
 
-    let end = emit::clock().now().unwrap();
+    let end = RUNTIME.clock().now().unwrap();
 
     // Write the results
-    let per_event = (end - start).as_nanos() as f64 / count as f64;
-
     stdout.emit(&emit::evt!(
         extent: start..end,
-        "emitted {count} events ({per_event}ns per run) with spawn {spawn} and flush {flush}",
+        "{count} iterations ({per_iteration}ns per iteration) with spawn {spawn} and flush {flush}",
         evt_kind: "span",
+        per_iteration: (end - start).as_nanos() as f64 / count as f64,
     ));
 
     reporter.emit_metrics(&stdout);
@@ -62,16 +72,16 @@ fn main() {
     drop(otelcol);
 }
 
-#[emit::span("test root")]
+#[emit::span(rt: &RUNTIME, "test root")]
 fn root(count: usize) {
     for i in 0..count {
         run(i);
     }
 }
 
-#[emit::span("test span {i}")]
+#[emit::span(rt: &RUNTIME, "test span {i}")]
 fn run(i: usize) {
-    emit::info!("test event {i}");
+    emit::info!(rt: &RUNTIME, "test event {i}");
 }
 
 struct OtelCol(Child);
